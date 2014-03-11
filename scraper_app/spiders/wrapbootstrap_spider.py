b8fb0f8981fbf67bec7e39f63fac01f862c98362
Vivek Agarwal <me@vivek.im>
2014-03-11 18:10:33 +0530

    Improve parsing.

--- scraper_app/spiders/wrapbootstrap_spider.py
+++ scraper_app/spiders/wrapbootstrap_spider.py
@@ -18,18 +18,18 @@ class WrapBootstrapSpider(CrawlSpider):
         Rule(SgmlLinkExtractor(allow=('themes/', 'tag/')), follow=True),
     )
 
-    def parse_template(self, response):
+    def parse(self, response):
         """
         Callback used by Scrapy to process downloaded responses
+        //*[@id="page_theme"]/div[2]/div/div/div/div[2]/div[4]/table/tbody/tr[10]/td[2]
         """
         response_body = response.body_as_unicode()
 
+        # Checking if coffee beans are present in the source, since it shifts down the divs
         coffee = True if 'cups of coffee' in response_body else False
-        released_div_pos = 10
-        if 'Uses Sass:' in response_body:
-            released_div_pos += 1
-        if 'Categories:' in response_body:
-            released_div_pos += 1
+
+        prop_xpath = '//div[@class="info_wrapper"]//tr[td[@class="key"]/strong/text() = "{}:"]/td[@class="value"]/text()'
+        substr_xpath = 'substring-after(normalize-space({}), "{}")'
 
         item_fields = {
             'item_hash': '//*[@id="offer_sku"]/text()',
@@ -37,11 +37,11 @@ class WrapBootstrapSpider(CrawlSpider):
             'thumbnail': '//*[@id="thing_image"]/@src',
             'description': '//*[@id="description"]',
             'creator': '//*[@id="product_manufacturer"]/text()',
-            'when': '//*[@id="page_theme"]/div[2]/div/div/div/div[2]/div[%d]/table/tbody/tr[%d]/td[2]/text()' % ((4, released_div_pos) if coffee else (3, released_div_pos)),
-            'bootstrap_version': 'substring-after(normalize-space(//*[@id="page_theme"]/div[2]/div/div/div/div[2]/div[{}]/table/tbody/tr[2]/td[2]/text()), "Compatible with ")'.format(4 if coffee else 3),
-            'cost_single': 'substring-after(normalize-space(//*[@id="page_theme"]/div[2]/div/div/div/div[2]/div[{}]/div[1]/span/text()), "$")'.format(3 if coffee else 2),
-            'cost_multiple': 'substring-after(normalize-space(//*[@id="page_theme"]/div[2]/div/div/div/div[2]/div[{}]/div[2]/a/span/text()), "$")'.format(3 if coffee else 2),
-            'cost_extended': 'substring-after(normalize-space(//*[@id="page_theme"]/div[2]/div/div/div/div[2]/div[{}]/div[3]/a/span/text()), "$")'.format(3 if coffee else 2),
+            'when': prop_xpath.format('Released'),
+            'bootstrap_version': substr_xpath.format(prop_xpath.format('Bootstrap'), 'Compatible with '),
+            'cost_single': substr_xpath.format('//*[@id="page_theme"]/div[2]/div/div/div/div[2]/div[{}]/div[1]//span/text()'.format(3 if coffee else 2), '$'),
+            'cost_multiple': substr_xpath.format('//*[@id="page_theme"]/div[2]/div/div/div/div[2]/div[{}]/div[2]//span/text()'.format(3 if coffee else 2), '$'),
+            'cost_extended': substr_xpath.format('//*[@id="page_theme"]/div[2]/div/div/div/div[2]/div[{}]/div[3]//span/text()'.format(3 if coffee else 2), '$'),
             'purchases': '//div[@class="purchases"]/span[@class="count"]/text()',
         }
 
